bad566de4149cc7167740cd7d854bcaeabff3ad8
Sagor <sagarsaha455@gmail.com>
2026-01-19 22:42:46 +0100

    Model evaluation is updated

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTraini
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
-'''
 from src.datascience.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
-'''
+
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -52,7 +51,7 @@ except Exception as e:
         raise e
 
 
-'''
+
 STAGE_NAME = "Model evaluation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -62,9 +61,3 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-
-
-
-
-
-'''
