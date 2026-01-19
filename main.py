ac814ebd3219297521362519b370adabe4ec048f
Sagor <sagarsaha455@gmail.com>
2026-01-19 21:59:04 +0100

    Data Transformation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
-'''
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+'''
 from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 from src.datascience.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 '''
@@ -27,7 +27,6 @@ except Exception as e:
         raise e
 
 
-'''
 
 STAGE_NAME = "Data Transformation stage"
 try:
@@ -39,6 +38,8 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+'''
 
 STAGE_NAME = "Model Trainer stage"
 try:
